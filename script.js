bd88f338b452090f45040929acd560aeadcec6a4
mohitpuri-codes <mohitpuri.goswami@simformsolutions.com>
2025-03-11 12:15:01 +0530

    feat: Added the keypress functionality

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+let inputStr = "";
+let display = document.querySelector(".display");
+
+document.addEventListener("keydown", backSpaceEventHandler);
+document.addEventListener("keypress", keyPressEventHandler);
+
+// function to handle the backspace
+function backSpaceEventHandler(e) {
+  if (e.key === "Backspace") {
+    inputStr = inputStr.slice(0, -1);
+    display.textContent = inputStr;
+  }
+}
+
+// function to handle keypress events
+function keyPressEventHandler(e) {
+  let allowedKeyPress = new Set([
+    "Enter",
+    "Backspace",
+    "(",
+    ")",
+    "*",
+    "-",
+    "+",
+    "/",
+    ".",
+    "=",
+  ]);
+  let key = e.key;
+
+  if ((key >= "0" && key <= "9") || allowedKeyPress.has(key)) {
+    if (key === "Enter" || key === "=") {
+      evaluate(inputStr);
+      inputStr = display.textContent;
+    } else {
+      if (inputStr === "Error") return;
+      inputStr = inputStr + key;
+      display.textContent = inputStr;
+    }
+  }
+}
+
+// function to evaluate the expression
+function evaluate(inputStr) {
+  try {
+    if (
+      inputStr === "function Error() { [native code] }" ||
+      inputStr === "Error"
+    )
+      return;
+    let result = eval(inputStr);
+    display.textContent = result.toFixed(3) ?? display.textContent;
+  } catch (e) {
+    display.textContent = "Error";
+  }
+}
+
+// dropdown functionality
 document
   .querySelector("#trigonometry-dropdown")
   .addEventListener("click", trigonometryFunction);
